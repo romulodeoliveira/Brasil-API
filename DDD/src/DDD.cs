9a378a5007c044869cec865e34ae86b6b9b03e59
Rômulo de Oliveira <devromulodeoliveira@gmail.com>
2023-05-16 10:19:55 -0300

    busca de cidades com ddd fornecido pela api

---
+++ DDD/src/DDD.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDD.src
+{
+    public class DDD
+    {
+        public string State { get; set; }
+        public List<string> Cities { get; set; }
+    }
+}
---
+++ DDD/src/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DDD.src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o DDD: ");
+            string ddd = Console.ReadLine();
+
+            Task.Run(async () =>
+            {
+                string url = $"https://brasilapi.com.br/api/ddd/v1/{ddd}";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        HttpResponseMessage response = await client.GetAsync(url);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string result = await response.Content.ReadAsStringAsync();
+
+                            DDD dddData = JsonConvert.DeserializeObject<DDD>(result);
+
+                            Console.WriteLine($"Estado: {dddData.State}");
+                            Console.WriteLine("Cidades:");
+                            for (int i = 0; i < dddData.Cities.Count; i++)
+                            {
+                                Console.WriteLine($"{i + 1}. {dddData.Cities[i]}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("A requisição à API retornou um erro.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+                    }
+                }
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
